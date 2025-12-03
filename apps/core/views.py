59ac9ac913ea733b363288e0b8184b8aba6cd091
LGJ0405 <rlfwls03@naver.com>
2025-12-03 10:12:54 +0900

    update

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
+from django.views.decorators.cache import never_cache
+from django.utils.decorators import method_decorator
 
 class LoginRequiredSessionMixin:
     login_url = "/accounts/login/"
 
+    @method_decorator(never_cache)
     def dispatch(self, request, *args, **kwargs):
         if not request.session.get("login_user_id"):
             return redirect(self.login_url)
