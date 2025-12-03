03eaea99f8e32d4f1754d236f1809aea473a0fa6
LGJ0405 <rlfwls03@naver.com>
2025-12-03 18:25:09 +0900

    update

--- final_django/settings/dev.py
+++ final_django/settings/dev.py
@@ -14,11 +14,3 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-# 개발 환경에서는 로컬 파일에 저장하도록 권장
-DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
-
-# 정적 파일은 base 설정 그대로 사용
-# MEDIA 설정 (로컬)
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
--- final_django/settings/prod.py
+++ final_django/settings/prod.py
@@ -10,8 +10,7 @@ ALLOWED_HOSTS = [
     os.getenv("DOMAIN_URL") or '',
 ]
 
-# 운영에서는 RDS(MySQL) 등을 쓰는 것을 권장
-# 기존 settings.py 안에 있던 주석을 그대로 활용
+# 운영은 RDS로
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -26,28 +25,3 @@ DATABASES = {
         },
     },
 }
-
-# 파일은 S3에 저장
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-STORAGES = {
-    "default": {
-        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
-    },
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
-
-AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
-AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME", "ap-northeast-2")
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
-AWS_LOCATION = 'media'
-AWS_QUERYSTRING_AUTH = False
-
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
