265aba0a262514e5ba9b9081ed6b7b8f3157f849
songeeeey <songeeee1004@gmail.com>
2025-12-05 18:15:13 +0900

    관리자 페이지추가

--- final_django/urls.py
+++ final_django/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('django_admin/', admin.site.urls),
     
     path("", include("apps.core.urls")),
     path("accounts/", include("apps.accounts.urls")),
