e9c42fa43e1a5cf279180c4632ec82b8e829fb30
LGJ0405 <rlfwls03@naver.com>
2025-12-05 17:12:40 +0900

    connect rds

--- final_django/settings/dev.py
+++ final_django/settings/dev.py
@@ -8,9 +8,23 @@ ALLOWED_HOSTS = [
 ]
 
 # 로컬은 sqlite3로 간단하게
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'testdb',          # 실제 DB 이름으로 수정
+        'USER': 'admin',           # 실제 유저명으로 수정
+        'PASSWORD': os.getenv('DATABASES_PASSWORD'),
+        'HOST': os.getenv('DATABASES_HOST'),
+        'PORT': '3306',
+        'OPTIONS': {
+            'sql_mode': 'STRICT_TRANS_TABLES',
+            'charset': 'utf8mb4',
+        },
+    },
 }
