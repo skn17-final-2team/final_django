d245cf8b2562cdabde9e53420bdbbb6bf8fef8b6
JAEEUN0129 <jennyuu0429@naver.com>
2025-12-08 11:20:40 +0900

    데브삭제

--- final_django/wsgi.py
+++ final_django/wsgi.py
@@ -11,7 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'final_django.settings.dev')    # 개발
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'final_django.settings.prod')   # 배포
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'final_django.settings')
 
 application = get_wsgi_application()
