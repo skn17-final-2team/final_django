afd6797a113d99484f61759fafbd1ce45b8a72b1
LGJ0405 <rlfwls03@naver.com>
2025-12-10 10:10:43 +0900

    초기 비밀번호 변경 모달

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
-from .views import login_view, login_api, logout_view, modify_pw_view,check_old_pw
+from .views import (
+    login_view,
+    login_api,
+    logout_view,
+    modify_pw_view,
+    check_old_pw,
+    modify_pw_initial,
+)
 
 app_name = "accounts"
 
@@ -9,4 +16,5 @@ urlpatterns = [
     path("logout/", logout_view, name="logout"),
     path("modify-pw/", modify_pw_view, name="modify_pw"),
     path("check-old-pw/", check_old_pw, name="check_old_pw"),
+    path("modify-pw-initial/", modify_pw_initial, name="modify_pw_initial"),
 ]
